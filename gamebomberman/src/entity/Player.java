35907a842aa30f27b9ad42db8909a0b55d513c3f
Truong09-karry <truonghk28@gmail.com>
2022-10-12 16:54:00 +0700

    Push lai hoi mau va YouWin nhe

---
+++ gamebomberman/src/entity/Player.java
@@ -0,0 +1,260 @@
+package entity;
+
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import main.GamePanel;
+import main.KeyHandler;
+import main.UtilityTool;
+import objects.OBJ_Bomb;
+import objects.OBJ_Fire;
+
+public class Player extends Entity {
+	KeyHandler keyH;
+
+	public final int screenX;
+	public final int screenY;
+
+	// Key
+	int hasKey = 0;
+
+	// dan bay theo 4 huong
+	public Player(GamePanel gp, KeyHandler keyH) {
+		super(gp);
+		this.keyH = keyH;
+
+		screenX = gp.screenWidth / 2 - (gp.tileSize / 2);
+		screenY = gp.screenHeight / 2 - (gp.tileSize / 2);
+
+		solidArea = new Rectangle(8,15,32,25);// nho hon player
+		solidAreaDefaultX = solidArea.x;
+		solidAreaDefaultY = solidArea.y;
+		setDefaultValues();
+		getPlayerImage();
+		bomb = new OBJ_Bomb(gp);
+		projectileUp = new OBJ_Fire(gp);
+		projectileDown = new OBJ_Fire(gp);
+		projectileLeft = new OBJ_Fire(gp);
+		projectileRight = new OBJ_Fire(gp);
+	}
+
+	public void setDefaultValues() {
+
+		worldX = gp.tileSize * 8;
+		worldY = gp.tileSize * 8;
+		speed = 7;
+		direction = "down";
+
+		// player status
+		maxLife = 6;
+		life = maxLife - 4;
+	}
+
+	public void getPlayerImage() {
+
+		up1 = setup("/player/up1");
+		up2 = setup("/player/up2");
+		down1 = setup("/player/down1");
+		down2 = setup("/player/down2");
+		left1 = setup("/player/left1");
+		left2 = setup("/player/left2");
+		right1 = setup("/player/right1");
+		right2 = setup("/player/right2");
+	}
+
+	public void update() {
+		if (keyH.upPressed || keyH.downPressed || keyH.leftPressed || keyH.rightPressed) {
+
+			if (keyH.upPressed == true) {
+				direction = "up";
+			}
+			if (keyH.downPressed == true) {
+				direction = "down";
+			}
+			if (keyH.leftPressed == true) {
+				direction = "left";
+			}
+			if (keyH.rightPressed == true) {
+				direction = "right";
+			}
+
+			// check va cham voi tuong
+			collisionOn = false;
+			gp.cChecker.checkTile(this);
+
+			// check va cham voi breakable tile
+			gp.cChecker.checkEntity(this, gp.bTile);
+
+			// check object collision
+			int objIndex = gp.cChecker.checkObject(this, true);
+			pickUpObject(objIndex);
+
+			// check va cham monster va Fire
+			int npcIndex = gp.cChecker.checkEntity(this, gp.npc);
+			interactNPC(npcIndex);
+
+			if (collisionOn == false) {
+				switch (direction) {
+				case "up":
+					worldY -= speed;
+					break;
+				case "down":
+					worldY += speed;
+					break;
+				case "left":
+					worldX -= speed;
+					break;
+				case "right":
+					worldX += speed;
+					break;
+				}
+			}
+
+			spriteCounter++;
+			if (spriteCounter > 8) {
+				if (spriteNum == 1)
+					spriteNum = 2;
+				else if (spriteNum == 2)
+					spriteNum = 1;
+
+				spriteCounter = 0;
+			}
+		}
+
+		if (gp.keyH.spacePressed == true && projectileDown.alive == false && projectileUp.alive == false
+				&& projectileLeft.alive == false && projectileRight.alive == false) {
+
+			// dat vi tri cho projectile
+			bombXpos = (worldX + gp.tileSize / 2) - ((worldX + gp.tileSize / 2) % gp.tileSize);
+			bombYpos = (worldY + gp.tileSize / 2) - ((worldY + gp.tileSize / 2) % gp.tileSize);
+			bomb.set(bombXpos, bombYpos, "down", true, this);
+			gp.projectileList.add(bomb);
+			projectileUp.set(bombXpos, bombYpos, "up", true, this);
+			projectileDown.set(bombXpos, bombYpos, "down", true, this);
+			projectileLeft.set(bombXpos, bombYpos, "left", true, this);
+			projectileRight.set(bombXpos, bombYpos, "right", true, this);
+			// delay thi bom no
+			new java.util.Timer().schedule(new java.util.TimerTask() {
+				@Override
+				public void run() {
+					// your code here
+					// them vao danh sach cac projectile
+					gp.projectileList.add(projectileUp);
+					gp.projectileList.add(projectileDown);
+					gp.projectileList.add(projectileLeft);
+					gp.projectileList.add(projectileRight);
+
+				}
+			},
+					// 2000
+					(bomb.maxLife / gp.FPS) * 1000);
+		}
+
+		// xu ly khi bi va cham voi quai
+		if (invincible == true) {
+			invincibleCounter++;
+			if (invincibleCounter > 120) {
+				invincible = false;
+				invincibleCounter = 0;
+			}
+		}
+		if (life <= 0) {
+			gp.gameState = gp.gameOverState;
+		}
+		if (gp.currentMap > 2) {
+			gp.gameState = gp.gameWinState;
+		}
+	}
+
+	public void pickUpObject(int i) {
+
+		if (i != 999) {
+			String name = gp.obj[gp.currentMap][i].name;// FIXED
+
+			switch (name) {
+			case "Speed":
+				speed += 1;
+				gp.obj[gp.currentMap][i] = null;// FIXED
+				break;
+			case "Key":
+				hasKey++;
+				gp.obj[gp.currentMap][i] = null;// FIXED
+				break;
+			case "Door":
+				if (hasKey > 0) {
+					gp.obj[gp.currentMap][i] = null;// FIXED
+					hasKey--;
+				}
+				break;
+			case "Tent":
+				gp.currentMap++;
+				gp.aSetter.setBreakableTile();
+				gp.aSetter.setObject();
+				gp.aSetter.setNPC();
+				break;
+			case "Heal":
+				life+=1;
+				gp.obj[gp.currentMap][i] = null;
+			}
+		}
+	}
+
+	public void interactNPC(int i) {
+		if (i != 999) {
+			if (invincible == false) {
+				speed = 4;
+				life--;
+				invincible = true;
+			}
+
+		}
+
+	}
+
+	public void draw(Graphics2D g2) {
+		BufferedImage image = null;
+
+		switch (direction) {
+		case "up":
+			if (spriteNum == 1)
+				image = up1;
+			if (spriteNum == 2)
+				image = up2;
+			break;
+		case "down":
+			if (spriteNum == 1)
+				image = down1;
+			if (spriteNum == 2)
+				image = down2;
+			break;
+		case "left":
+			if (spriteNum == 1)
+				image = left1;
+			if (spriteNum == 2)
+				image = left2;
+			break;
+		case "right":
+			if (spriteNum == 1)
+				image = right1;
+			if (spriteNum == 2)
+				image = right2;
+			break;
+		}
+
+		// lam mo nhan vat khi bi giet
+		if (invincible == true) {
+			g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.3f));
+		}
+
+		g2.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
+
+		// reset alpha
+		g2.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
+	}
+}
---
+++ gamebomberman/src/main/AssetSetter.java
@@ -0,0 +1,210 @@
+package main;
+
+import breakableTiles.BreakableWall;
+import entity.NPC_Orc;
+import objects.OBJ_Door;
+import objects.OBJ_Heal;
+import objects.OBJ_Key;
+import objects.OBJ_Speed;
+import objects.OBJ_Tent;
+
+public class AssetSetter {
+
+	GamePanel gp;
+
+	public AssetSetter(GamePanel gp) {
+		this.gp = gp;
+	}
+
+	public void setObject() {
+		if (gp.currentMap == 0) {
+			gp.obj[0][1] = new OBJ_Speed(gp);
+			gp.obj[0][1].worldX = 7 * gp.tileSize;
+			gp.obj[0][1].worldY = 9 * gp.tileSize;
+			gp.obj[0][2] = new OBJ_Speed(gp);
+			gp.obj[0][2].worldX = 6 * gp.tileSize;
+			gp.obj[0][2].worldY = 4 * gp.tileSize;
+			gp.obj[0][3] = new OBJ_Speed(gp);
+			gp.obj[0][3].worldX = 15 * gp.tileSize;
+			gp.obj[0][3].worldY = 42 * gp.tileSize;
+			gp.obj[0][4] = new OBJ_Key(gp);
+			gp.obj[0][4].worldX = 1 * gp.tileSize;
+			gp.obj[0][4].worldY = 1 * gp.tileSize;
+			gp.obj[0][5] = new OBJ_Key(gp);
+			gp.obj[0][5].worldX = 48 * gp.tileSize;
+			gp.obj[0][5].worldY = 2 * gp.tileSize;
+			gp.obj[0][6] = new OBJ_Door(gp);
+			gp.obj[0][6].worldX = 6 * gp.tileSize;
+			gp.obj[0][6].worldY = 3 * gp.tileSize;
+			gp.obj[0][7] = new OBJ_Door(gp);
+			gp.obj[0][7].worldX = 9 * gp.tileSize;
+			gp.obj[0][7].worldY = 27 * gp.tileSize;
+			gp.obj[0][8] = new OBJ_Tent(gp);
+			gp.obj[0][8].worldX = 25 * gp.tileSize;
+			gp.obj[0][8].worldY = 25 * gp.tileSize;
+			gp.obj[0][9] = new OBJ_Heal(gp);
+			gp.obj[0][9].worldX = 1 * gp.tileSize;
+			gp.obj[0][9].worldY = 4 * gp.tileSize;
+		}
+		if (gp.currentMap == 1) {
+			gp.obj[1][1] = new OBJ_Speed(gp);
+			gp.obj[1][1].worldX = 4 * gp.tileSize;
+			gp.obj[1][1].worldY = 4 * gp.tileSize;
+			gp.obj[1][2] = new OBJ_Speed(gp);
+			gp.obj[1][2].worldX = 15 * gp.tileSize;
+			gp.obj[1][2].worldY = 40 * gp.tileSize;
+			gp.obj[1][3] = new OBJ_Speed(gp);
+			gp.obj[1][3].worldX = 33 * gp.tileSize;
+			gp.obj[1][3].worldY = 47 * gp.tileSize;
+			gp.obj[1][4] = new OBJ_Key(gp);
+			gp.obj[1][4].worldX = 45 * gp.tileSize;
+			gp.obj[1][4].worldY = 3 * gp.tileSize;
+			gp.obj[1][5] = new OBJ_Key(gp);
+			gp.obj[1][5].worldX = 42 * gp.tileSize;
+			gp.obj[1][5].worldY = 7 * gp.tileSize;
+			gp.obj[1][11] = new OBJ_Key(gp);
+			gp.obj[1][11].worldX = 3 * gp.tileSize;
+			gp.obj[1][11].worldY = 34 * gp.tileSize;
+			gp.obj[1][9] = new OBJ_Key(gp);
+			gp.obj[1][9].worldX = 4 * gp.tileSize;
+			gp.obj[1][9].worldY = 44 * gp.tileSize;
+			gp.obj[1][12] = new OBJ_Key(gp);
+			gp.obj[1][12].worldX = 2 * gp.tileSize;
+			gp.obj[1][12].worldY = 36 * gp.tileSize;
+			gp.obj[1][10] = new OBJ_Door(gp);
+			gp.obj[1][10].worldX = 4 * gp.tileSize;
+			gp.obj[1][10].worldY = 39 * gp.tileSize;
+
+			gp.obj[1][6] = new OBJ_Door(gp);
+			gp.obj[1][6].worldX = 47 * gp.tileSize;
+			gp.obj[1][6].worldY = 6 * gp.tileSize;
+			gp.obj[1][7] = new OBJ_Door(gp);
+			gp.obj[1][7].worldX = 37 * gp.tileSize;
+			gp.obj[1][7].worldY = 7 * gp.tileSize;
+			gp.obj[1][8] = new OBJ_Tent(gp);
+			gp.obj[1][8].worldX = 26 * gp.tileSize;
+			gp.obj[1][8].worldY = 44 * gp.tileSize;
+			gp.obj[1][11] = new OBJ_Door(gp);
+			gp.obj[1][11].worldY = 20 * gp.tileSize;
+			gp.obj[1][11].worldX = 45 * gp.tileSize;
+
+		}
+		if (gp.currentMap == 2) {
+			gp.obj[2][1] = new OBJ_Speed(gp);
+			gp.obj[2][1].worldX = 18 * gp.tileSize;
+			gp.obj[2][1].worldY = 24 * gp.tileSize;
+			gp.obj[2][2] = new OBJ_Speed(gp);
+			gp.obj[2][2].worldX = 16 * gp.tileSize;
+			gp.obj[2][2].worldY = 15 * gp.tileSize;
+			gp.obj[2][3] = new OBJ_Speed(gp);
+			gp.obj[2][3].worldX = 42 * gp.tileSize;
+			gp.obj[2][3].worldY = 36 * gp.tileSize;
+			gp.obj[2][4] = new OBJ_Key(gp);
+			gp.obj[2][4].worldX = 47 * gp.tileSize;
+			gp.obj[2][4].worldY = 34 * gp.tileSize;
+			gp.obj[2][5] = new OBJ_Key(gp);
+			gp.obj[2][5].worldX = 29 * gp.tileSize;
+			gp.obj[2][5].worldY = 35 * gp.tileSize;
+			gp.obj[2][6] = new OBJ_Door(gp);
+			gp.obj[2][6].worldX = 28 * gp.tileSize;
+			gp.obj[2][6].worldY = 42 * gp.tileSize;
+			gp.obj[2][7] = new OBJ_Door(gp);
+			gp.obj[2][7].worldX = 5 * gp.tileSize;
+			gp.obj[2][7].worldY = 18 * gp.tileSize;
+			gp.obj[2][8] = new OBJ_Tent(gp);
+			gp.obj[2][8].worldX = 20 * gp.tileSize;
+			gp.obj[2][8].worldY = 20 * gp.tileSize;
+
+		}
+
+	}
+
+	public void setNPC() {
+		if (gp.currentMap == 0) {
+			gp.npc[0][0] = new NPC_Orc(gp);
+			gp.npc[0][0].worldX = gp.tileSize * 4;
+			gp.npc[0][0].worldY = gp.tileSize * 7;
+			gp.npc[0][2] = new NPC_Orc(gp);
+			gp.npc[0][2].worldX = gp.tileSize * 5;
+			gp.npc[0][2].worldY = gp.tileSize * 43;
+			gp.npc[0][3] = new NPC_Orc(gp);
+			gp.npc[0][3].worldX = gp.tileSize * 3;
+			gp.npc[0][3].worldY = gp.tileSize * 7;
+
+			gp.npc[0][4] = new NPC_Orc(gp);
+			gp.npc[0][4].worldX = gp.tileSize * 9;
+			gp.npc[0][4].worldY = gp.tileSize * 43;
+			gp.npc[0][5] = new NPC_Orc(gp);
+			gp.npc[0][5].worldX = gp.tileSize * 38;
+			gp.npc[0][5].worldY = gp.tileSize * 45;
+			gp.npc[0][6] = new NPC_Orc(gp);
+			gp.npc[0][6].worldX = gp.tileSize * 38;
+			gp.npc[0][6].worldY = gp.tileSize * 26;
+		}
+
+		if (gp.currentMap == 1) {
+			gp.npc[1][0] = new NPC_Orc(gp);
+			gp.npc[1][0].worldX = gp.tileSize * 7;
+			gp.npc[1][0].worldY = gp.tileSize * 10;
+			gp.npc[1][1] = new NPC_Orc(gp);
+			gp.npc[1][1].worldX = gp.tileSize * 5;
+			gp.npc[1][1].worldY = gp.tileSize * 8;
+			gp.npc[1][2] = new NPC_Orc(gp);
+			gp.npc[1][2].worldX = gp.tileSize * 5;
+			gp.npc[1][2].worldY = gp.tileSize * 34;
+			gp.npc[1][3] = new NPC_Orc(gp);
+			gp.npc[1][3].worldX = gp.tileSize * 3;
+			gp.npc[1][3].worldY = gp.tileSize * 32;
+
+			gp.npc[1][4] = new NPC_Orc(gp);
+			gp.npc[1][4].worldX = gp.tileSize * 33;
+			gp.npc[1][4].worldY = gp.tileSize * 44;
+			gp.npc[1][5] = new NPC_Orc(gp);
+			gp.npc[1][5].worldX = gp.tileSize * 35;
+			gp.npc[1][5].worldY = gp.tileSize * 44;
+			gp.npc[1][6] = new NPC_Orc(gp);
+			gp.npc[1][6].worldX = gp.tileSize * 27;
+			gp.npc[1][6].worldY = gp.tileSize * 33;
+		}
+//		if (gp.currentMap == 2) {
+//
+//		}
+//		if (gp.currentMap == 3) {
+//			gp.npc[3][0] = new NPC_Orc(gp);
+//			gp.npc[3][0].worldX = gp.tileSize * 9;
+//			gp.npc[3][0].worldY = gp.tileSize * 10;
+//			gp.npc[3][1] = new NPC_Orc(gp);
+//			gp.npc[3][1].worldX = gp.tileSize * 8;
+//			gp.npc[3][1].worldY = gp.tileSize * 8;
+//		}
+
+	}
+
+	public void setBreakableTile() {
+		int i = 0;
+
+		int col = 0;
+		int row = 0;
+		int x = 0;
+		int y = 0;
+
+		while (col < gp.maxWorldCol && row < gp.maxWorldRow) {
+			// lay ra so luu trong mapTileNum, neu la so 2 thi tao 1 cai breakable wall
+			int tileNum = gp.tileM.mapTileNum[gp.currentMap][col][row];
+
+			if (tileNum == 2) {
+				gp.bTile[gp.currentMap][i] = new BreakableWall(gp);
+				gp.bTile[gp.currentMap][i].worldX = gp.tileSize * col;
+				gp.bTile[gp.currentMap][i].worldY = gp.tileSize * row;
+				i++;
+			}
+			col++;
+
+			if (col == gp.maxWorldCol) {
+				col = 0;
+				row++;
+			}
+		}
+	}
+
+}
